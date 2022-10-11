0bf47ffac8585ca16fbdf909f1e9b7eb4742f9ef
Hunter <hunterwhite.dev@gmail.com>
2022-10-10 20:47:36 -0400

    User Profile Body Structure finished

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./UserProfile.css";
 import { useParams } from "react-router-dom";
-
+import ComputerIcon from "@mui/icons-material/Computer";
+import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
+import ConnectedTvIcon from "@mui/icons-material/ConnectedTv";
+import VideocamIcon from "@mui/icons-material/Videocam";
 function UserProfile({ user }) {
   console.log(user);
   return (
@@ -27,7 +30,50 @@ function UserProfile({ user }) {
         </div>
       </div>
 
-      <div className="userProfile__body">BODY</div>
+      <div className="userProfile__body">
+        <div className="userProfile__bodyItemContainer">
+          <ComputerIcon />
+          <p>Total Time</p>
+          <p>
+            <em>{user.TotalTime.Days}</em> Days <em>{user.TotalTime.Hours}</em>{" "}
+            Hours <em>{user.TotalTime.Minutes}</em> Minutes{" "}
+            <em>{user.TotalTime.Seconds}</em> Seconds
+          </p>
+        </div>
+
+        <div className="userProfile__bodyItemContainer">
+          <HeadsetMicIcon />
+          <p>Total Active Time</p>
+          <p>
+            <em>{user.ActiveTime.Days}</em> Days{" "}
+            <em>{user.ActiveTime.Hours}</em> Hours{" "}
+            <em>{user.ActiveTime.Minutes}</em> Minutes{" "}
+            <em>{user.ActiveTime.Seconds}</em> Seconds
+          </p>
+        </div>
+
+        <div className="userProfile__bodyItemContainer">
+          <ConnectedTvIcon />
+          <p>Total Strean Time</p>
+          <p>
+            <em>{user.StreamTime.Days}</em> Days{" "}
+            <em>{user.StreamTime.Hours}</em> Hours{" "}
+            <em>{user.StreamTime.Minutes}</em> Minutes{" "}
+            <em>{user.StreamTime.Seconds}</em> Seconds
+          </p>
+        </div>
+
+        <div className="userProfile__bodyItemContainer">
+          <VideocamIcon />
+          <p>Total WebCam Time</p>
+          <p>
+            <em>{user.WebCamTime.Days}</em> Days{" "}
+            <em>{user.WebCamTime.Hours}</em> Hours{" "}
+            <em>{user.WebCamTime.Minutes}</em> Minutes{" "}
+            <em>{user.WebCamTime.Seconds}</em> Seconds
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
